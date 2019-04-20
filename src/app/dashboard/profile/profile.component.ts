0aa09f46418798cbf830dd98b1805ff3165e31e6
Omar Atta <omaratta212@gmail.com>
2019-04-20 05:49:20 +0200

    Profile

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
   }
-
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.background = '#fff';
+  }
 }
